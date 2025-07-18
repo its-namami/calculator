a695ab075ed7cf8a88e73219e6e67ceec1be9ad0
its-namami <164639260+its-namami@users.noreply.github.com>
2025-07-19 01:38:10 +0200

    feat: add fully functioning sqrt method and error handling for it
    It uses BigInt on the inside, and warns the user if a number was passed,
    because it automatically converts it to the scientific notation, where
    precision gets less.
    The current sqrt implementation is also not 100% perfect, as the last
    couple digits are almost certainly incorrect, which happens due to
    irrational numbers not having the exact sqrt value. Thus - the precision
    is set huge (17 digits as of now).

--- assets/scripts/decimal.js
+++ assets/scripts/decimal.js
@@ -8,6 +8,13 @@ class Decimal {
     if (!Number.isFinite(Number(number))) return Decimal.#error('badNum', number);
 
     let numberString = number.toString();
+
+    if (numberString.indexOf('e') !== -1) {
+      console.log(numberString, numberString.indexOf('e'));
+      Decimal.#warning('inputScientificNotation', numberString);
+      numberString = Number(numberString).toString();
+    }
+
     let decimalPointPosition = numberString.indexOf('.') + 1;
 
     this.#absDigits = numberString.replace(/-|\./g, '');
@@ -24,14 +31,17 @@ class Decimal {
   get value() {
     return Decimal.#normalize(this.#rawDigits, this.#decimalLen);
   }
-
+  // Due to the binary search algorithm, the square root's
+  // value's last digits are almost certainly wrong,
+  // thus this high precision
   sqrt() {
     const firstDecimal = this.#getCopy();
 
-    const precision = BigInt(15);
-    const seeking = BigInt(firstDecimal.value) * BigInt(10) ** precision;
+    const precision = BigInt(17);
+    console.log(firstDecimal);
+    let seeking = BigInt(firstDecimal.#rawDigits) * BigInt(10) ** precision;
     let lowerBound = BigInt(1) * BigInt(10) ** (precision);
-    let upperBound = BigInt(firstDecimal.value) * BigInt(10) ** (precision);
+    let upperBound = BigInt(firstDecimal.#rawDigits) * BigInt(10) ** (precision);
     let tolerance = BigInt(10) ** BigInt(firstDecimal.#rawDigits.length);
 
     const calc = {
@@ -43,30 +53,57 @@ class Decimal {
       }
     };
 
-    // should be "while"-loop, once I grasp the logic
-    for (let i = 0; i < 500; i++) {
-      console.log(`Current variables:\nAvg: ${calc.avg}\nAvgSq: ${calc.avgsq}\nSeeking: ${seeking}\nRange: ${lowerBound}~${upperBound}`);
+    if (firstDecimal.#absDigits === '0' || firstDecimal.#absDigits === '') return 0;
 
-      if (calc.avgsq + tolerance > seeking && calc.avgsq - tolerance < seeking) {
-        console.info(`Found it! The square root of ${seeking} is: ${calc.avg}`);
-        console.info(`Iterations it took: ${i}`);
-        break;
-      } else {
-        if (calc.avgsq > seeking) {
-          console.info(`Need less, because ${calc.avgsq} > ${seeking}`);
-          upperBound = calc.avg;
+    if (firstDecimal.value[0] !== '0' && !firstDecimal.#isNegative) {
+      let i = 0;
+
+      while (i < 500) {
+        i++;
+
+        if (calc.avgsq >= seeking && calc.avgsq - tolerance < seeking) {
+          break;
         } else {
-          console.info(`Need more, because ${calc.avgsq} < ${seeking}`);
-          lowerBound = calc.avg;
+          if (calc.avgsq > seeking) {
+            upperBound = calc.avg;
+          } else {
+            lowerBound = calc.avg;
+          }
         }
       }
+
+      if (i === 500) Decimal.#warning('sqrtOverItterated');
+      return Decimal.from(Decimal.#normalize(calc.avg, calc.avg.toString().length - Math.round(firstDecimal.#absDigits.length / 2)));
+
+    } else if (!firstDecimal.#isNegative) {
+      seeking = seeking * BigInt(10) ** BigInt(firstDecimal.#decimalLen);
+      upperBound = seeking;
+      let i = 0;
+
+      while (i < 500) {
+        i++;
+
+        if (calc.avgsq >= seeking && calc.avgsq - tolerance < seeking) {
+        } else {
+          if (calc.avgsq > seeking) {
+            upperBound = calc.avg;
+          } else {
+            lowerBound = calc.avg;
+          }
+        }
+      }
+
+      if (i === 500) Decimal.#warning('sqrtOverItterated');
+      return Decimal.from(Decimal.#normalize(calc.avg, calc.avg.toString().length + firstDecimal.#absDigits.length / 2 - 1));
+    } else {
+      return Decimal.#error('negSqrt');
     }
   }
 
   add(secondDecimal) {
     const firstDecimal = this.#getCopy();
 
-    if (secondDecimal instanceof Decimal === false) secondDecimal = new Decimal(secondDecimal);
+    if (secondDecimal instanceof Decimal === false) secondDecimal = Decimal.from(secondDecimal);
 
     const maxLen = Math.max(firstDecimal.#absDigits.length, secondDecimal.#absDigits.length);
     const maxDecimalLen = Math.max(firstDecimal.#decimalLen, secondDecimal.#decimalLen);
@@ -74,24 +111,24 @@ class Decimal {
     const secondDecimalValue = BigInt(secondDecimal.#growDecimal(maxDecimalLen));
     const numberResult = firstDecimalValue + secondDecimalValue;
 
-    return new Decimal(Decimal.#normalize(numberResult, maxDecimalLen));
+    return Decimal.from(Decimal.#normalize(numberResult, maxDecimalLen));
   }
 
   multiply(secondDecimal) {
     const firstDecimal = this.#getCopy();
 
-    if (secondDecimal instanceof Decimal === false) secondDecimal = new Decimal(secondDecimal);
+    if (secondDecimal instanceof Decimal === false) secondDecimal = Decimal.from(secondDecimal);
 
     const sumLen = firstDecimal.#decimalLen + secondDecimal.#decimalLen;
     const numberResult = BigInt(firstDecimal.#rawDigits) * BigInt(secondDecimal.#rawDigits);
 
-    return new Decimal(Decimal.#normalize(numberResult, sumLen));
+    return Decimal.from(Decimal.#normalize(numberResult, sumLen));
   }
 
   divide(secondDecimal) {
     const firstDecimal = this.#getCopy();
 
-    if (secondDecimal instanceof Decimal === false) secondDecimal = new Decimal(secondDecimal);
+    if (secondDecimal instanceof Decimal === false) secondDecimal = Decimal.from(secondDecimal);
 
     const maxLen = Math.max(firstDecimal.#absDigits.length, secondDecimal.#absDigits.length);
     const maxDecimalLen = Math.max(firstDecimal.#decimalLen, secondDecimal.#decimalLen);
@@ -114,11 +151,20 @@ class Decimal {
 
     const result = (newFirstDecimalValue / secondDecimalValue).toString();
 
-    return new Decimal(Decimal.#normalize(result, decimals));
+    return Decimal.from(Decimal.#normalize(result, decimals));
+  }
+
+  static from(input) {
+    if (typeof input === "number") {
+      Decimal.#warning('inputNumber');
+      return new Decimal(input.toString());
+    } else {
+      return new Decimal(input);
+    }
   }
 
   #getCopy() {
-    return new Decimal(this.value);
+    return Decimal.from(this.value);
   }
 
   #growDecimal(scale) {
@@ -132,13 +178,28 @@ class Decimal {
   static #error(error = 'default', info = undefined) {
     switch (error) {
       case 'badNum':
-        throw new Error (`Incorrect number: ${info}!`);
+        throw new Error (`\nIncorrect number: ${info}!`);
       case 'badScale':
-        throw new Error (`Cannot have less than 0 decimals: (${info})!`);
+        throw new Error (`\nCannot have less than 0 decimals: (${info})!`);
       case 'divideZero':
-        throw new Error ('Cannot divide by zero!');
+        throw new Error ('\nCannot divide by zero!');
+      case 'negSqrt':
+        throw new Error ('\nSquare root of a negative number is imaginary!');
       case 'default':
-        throw new Error ('Fatal error, that\'s all we know...');
+        throw new Error ("\nFatal error, that's all we know...");
+    }
+  }
+
+  static #warning(warning = 'default', info = undefined) {
+    switch (warning) {
+      case 'inputNumber':
+        console.warn(`Don't use numbers: please pass as string or bigint instead.`);
+        break;
+      case 'inputScientificNotation':
+        console.warn(`Loss of precision: the number was parsed in the scientific notation ${info}\nPlease pass as string or bigint instead.`);
+        break;
+      case 'default':
+        console.warn("There's some warning, that's all we know...");
     }
   }
 
