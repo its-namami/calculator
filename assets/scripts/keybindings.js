222d63af56d883f0884a4dd73ea4c7ddd2350a96
its-namami <164639260+its-namami@users.noreply.github.com>
2025-10-11 23:36:28 +0200

    feat: remove unused signs, rework keybindings

--- assets/scripts/keybindings.js
+++ assets/scripts/keybindings.js
@@ -1,223 +1,38 @@
-import calculator from './calculator.js';
-// Here is better Idea: 
-// Have in each Keybind-Object (for each number or whatever)
-// the Object "key", "shiftDown", "ctrlDown"
-// and in the loop specify if hasOwnProperty shiftDown and is true
-// then bla bla bla. If fore some reason that property is false,
-// when setting in the bindings to prevent unaccuracy and ambiguity
-// make that key only work when shiftDown or something is false.
-const keyBindings = {
-  numbers: {
-    // function: function(arg) {
-    //   calculator.addNumber(arg);
-    // },
-    0: [
-      {
-	key: '0',
-      },
+export default class Keybindings {
+  static #keys = new Map([
+    [[['0'], ['p']], '0'],
+    [[['1'], ['q']], '1'],
+    [[['2'], ['w']], '2'],
+    [[['3'], ['e']], '3'],
+    [[['4'], ['r']], '4'],
+    [[['5'], ['t']], '5'],
+    [[['6'], ['y']], '6'],
+    [[['7'], ['u']], '7'],
+    [[['8'], ['i']], '8'],
+    [[['9'], ['o']], '9'],
+    [[['.'], [',']], '.'],
+    [[['-'], ['m', 'Shift']], '-'],
+    [[['√'], ['s', 'Shift']], '√'],
+    [[['/'], ['/', 'Shift'], ['d', 'Shift']], '/'],
+    [[['+'], ['+', 'Shift'], ['p', 'Shift']], '+'],
+    [[['*'], ['*', 'Shift'], ['t', 'Shift']], '*'],
+    [[['='], ['e', 'Shift'], ['Enter']], '='],
+    [[['Backspace'], ['x', 'Shift']], 'delete'],
+    [[['Escape'], ['c', 'Control']], 'clear'],
+  ]);
 
-      {
-	key: 'o',
-      },
-    ],
-    1: [
-      {
-	key: '1',
-      },
-    ],
-    2: [
-      {
-	key: '2',
-      },
-    ],
-    3: [
-      {
-	key: '3',
-      },
-    ],
-    4: [
-      {
-	key: '4',
-      },
-    ],
-    5: [
-      {
-	key: '5',
-      },
-    ],
-    6: [
-      {
-	key: '6',
-      },
-    ],
-    7: [
-      {
-	key: '7',
-      },
-    ],
-    8: [
-      {
-	key: '8',
-      },
-    ],
-    9: [
-      {
-	key: '9',
-      },
-    ],
-  },
-
-  operators: {
-    // function: function(arg) {
-    //   calculator.addOperator(arg);
-    // },
-    'oper-minus': [
-      {
-	key: '-',
-      },
-
-      {
-	key: 's',
-      },
-    ],
-    'oper-plus': [
-      {
-	key: '+',
-      },
-
-      {
-	key: 'p',
-      },
-    ],
-    'oper-divide': [
-      {
-	key: '/',
-      },
-
-      {
-	key: 'd',
-      },
-    ],
-    'oper-multiply': [
-      {
-	key: '*',
-      },
-
-      {
-	key: 'x',
-      },
-
-      {
-	key: 'm',
-      },
-    ],
-    'oper-exponent': [
-      {
-	key: '^',
-      },
-
-      {
-	key: 'e',
-      },
-    ],
-  },
-
-  special: {
-    'deleteDigit': [
-      {
-	'ctrlKey': false,
-	'shiftKey': false,
-	key: 'Backspace',
-      },
-    ],
-    'clearEntry': [
-      {
-	'ctrlKey': false,
-	'shiftKey': true,
-	key: 'Backspace',
-      },
-    ],
-    'clearAll': [
-      {
-	'ctrlKey': true,
-	'shiftKey': true,
-	key: 'Backspace',
-      },
-    ],
-  },
-
-  other: {
-    'decimalSign': [
-      {
-	key: '.',
-      },
-    ],
-    'equalSign': [
-      {
-	key: '=',
-      },
-
-      {
-	key: 'Enter',
-      },
-
-      {
-	key: 'c',
-	ctrlKey: true,
-      },
-    ],
-  },
-}
-
-const keyCheck = (event, keybindings = keyBindings) => {
-  // console.log(keyBindings);
-  Object.entries(keybindings).forEach(property => {
-    const keyCategory = property[0];
-    Object.entries(property[1]).forEach(eliteProperty => {
-      const numberOutput = eliteProperty[0];
-      eliteProperty[1].forEach(keyBinding => {
-	let allCorrectKeysPressed = true;
-
-	Object.entries(keyBinding).forEach(keyDown => {
-	  const keyName = keyDown[0];
-	  const keyValue = keyDown[1];
-
-	  if (keyName === 'shiftKey') {
-	    if (event.shiftKey !== keyValue) allCorrectKeysPressed = false;
-	  } else if (keyName === 'ctrlKey') {
-	    if (event.ctrlKey !== keyValue) allCorrectKeysPressed = false;
-	  } else if (keyName === 'key') {
-	    if (event.key !== keyValue) allCorrectKeysPressed = false;
-	  }
-	});
-	if (keyCategory === 'numbers') {
-	  if (allCorrectKeysPressed) {
-	    calculator.addNumber(numberOutput)
-	  }
-	} else if (keyCategory === 'operators') {
-	  if (allCorrectKeysPressed) {
-	    if (calculator.operator === undefined) {
-	      if (calculator.number1 === '' && numberOutput === 'oper-minus') calculator.addNumber('-');
-	      else calculator.addOperator(numberOutput);
-	    } else {
-	      calculator.calculate();
-	      calculator.addOperator(numberOutput);
-	    }
-	  }
-	} else if (keyCategory === 'special') {
-	  if (allCorrectKeysPressed) {
-	    if (numberOutput === 'deleteDigit') calculator.deleteLastDigit();
-	    if (numberOutput === 'clearEntry') calculator.reset();
-	  }
-	} else if (keyCategory === 'other') {
-	  if (allCorrectKeysPressed) {
-	    if (numberOutput === 'decimalSign') calculator.addDecimalSign();
-	    if (numberOutput === 'equalSign') calculator.calculate();
-	  }
+  static keyProcessor(key, shift = false, control = false) {
+    for (const [keybindings, result] of Keybindings.#keys) {
+      for (const keybinding of keybindings) {
+	if (keybinding.includes(key.toLowerCase())
+	    && key !== 'Shift'
+	    && key !== 'Control'
+	    && shift === keybinding.includes('Shift')
+	    && control === keybinding.includes('Control')
+	) {
+	  return result;
 	}
-      });
-    });
-  });
-};
-
-export { keyBindings, keyCheck };
+      }
+    }
+  }
+}
